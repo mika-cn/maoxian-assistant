933800cb80030bb3ed6610cd287b769f645ca216
mika <mika@nothing.org>
2021-10-08 15:00:19 +0800

    Enhance plan structure
      * rename type attribute
      * fix attribute validation
      * fix plans

--- lib/mx-plan-action.rb
+++ lib/mx-plan-action.rb
@@ -2,15 +2,28 @@ require_relative 'json-object'
 
 class MxPlanAction
   include JsonObject
-  TYPES = %w(self.replace self.attr parent.attr self.add self.remove)
+  TYPES = [ "__IGNORE_ME__",
+
+    "assign.from.self-attr",
+    "assign.from.parent-attr",
+    "assign.from.ancestor-attr",
+    "assign.from.ancestor.child-attr",
+
+    "replace.last-match",
+    "replace.all",
+
+    "split2list.add",
+    "split2list.remove",
+  ]
 
   required_attr :type, in: TYPES
   required_attr :pick
-  required_attr :attr,   if: -> {!['self.add', 'self.remove'].include?(type)}
-  required_attr :subStr, if: -> {type == 'self.replace'}
-  required_attr :newStr, if: -> {type == 'self.replace'}
-  required_attr :tAttr,  if: -> {['self.attr', 'parent.attr'].include?(type)}
-  required_attr :value,  if: -> {['self.add', 'self.remove'].include?(type)}
-  optional_attr :sep
+  required_attr :attr
+  required_attr :subStr,    if: -> {TYPES[5..6].include?(type)}
+  required_attr :newStr,    if: -> {TYPES[5..6].include?(type)}
+  required_attr :tElem,     if: -> {TYPES[3..4].include?(type)}
+  required_attr :tAttr,     if: -> {TYPES[1..4].include?(type)}
+  required_attr :value,     if: -> {TYPES[7..8].include?(type)}
+  optional_attr :sep,       if: -> {TYPES[7..8].include?(type)}
 end
 
